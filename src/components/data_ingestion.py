745326a047bbd7eb1dd43daf81b53241249a54c1
lokeshlinga86 <lokesh.linga86@gmail.com>
2025-10-27 16:50:56 -0400

    add files

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn .model_selection import train_test_split
 from dataclasses import dataclass
-
+##################
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts',"train.csv")
     test_data_path:str=os.path.join('artifacts',"test.csv")
